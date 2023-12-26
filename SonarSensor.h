c90f6476ca9a7b248a593128fc1049d9c7ce2521
Yusuf  Eren Hoşgör <119801795+wicklash@users.noreply.github.com>
2023-12-26 18:41:43 +0300

    Add files via upload

---
+++ SonarSensor.h
@@ -0,0 +1,23 @@
+// SonarSensor.h
+#ifndef SONAR_SENSOR_H
+#define SONAR_SENSOR_H
+
+
+
+class SonarSensor {
+private:
+    double ranges[2];
+    NAORobotAPI* robotAPI;
+
+public:
+    SonarSensor(NAORobotAPI* _robotAPI);
+
+    double getRange(int index) const;
+    double getMax(int& index) const;
+    double getMin(int& index) const;
+    void updateSensor( double newRanges[]);
+
+    double operator[](int index) const;
+};
+
+#endif // SONAR_SENSOR_H
