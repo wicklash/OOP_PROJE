5709ea26c434e2a68c995b4bae5f564eb94231e3
saliheu <147442085+saliheu@users.noreply.github.com>
2023-12-27 19:31:19 +0300

    Add files via upload

--- ForceSensor.h
+++ ForceSensor.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef FORCESENSOR_H
+#define FORCESENSOR_H
+
+#include "NaoRobotAPI.h"
+
+class ForceSensor {
+private:
+    double force;
+    NaoRobotAPI* robotAPI;
+
+public:
+    // Kurucu fonksiyon
+    ForceSensor(NaoRobotAPI* api);
+
+    // Sensörü günceller
+    void updateSensor(double force);
+
+    // Sensörden kuvvet deðerini alýr
+    double getForce();
+
+    // Robotun düþüp düþmediðini kontrol eder
+    bool checkFall();
+};
+
+#endif // !FORCESENSOR_H
